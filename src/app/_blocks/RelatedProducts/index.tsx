c918ffbbb6740c1643b1446897d98684c3ca1b1d
freshposh-code <atomisefarouk919@gmail.com>
2023-12-24 04:24:05 +0100

    first commit

--- src/app/_blocks/RelatedProducts/index.tsx
+++ src/app/_blocks/RelatedProducts/index.tsx
@@ -22,10 +22,10 @@ export const RelatedProducts: React.FC<RelatedProductsProps> = props => {
       <Gutter>
         <h3 className={classes.title}>Related Product</h3>
         <div className={classes.grid}>
-          {docs?.map((doc, index) => {
+          {docs?.map((doc) => {
             if (typeof doc === 'string') return null
 
-            return  <Card relationTo={relationTo} doc={doc} showCategories />
+            return  <Card key={doc.id} relationTo={relationTo} doc={doc} showCategories />
 
             
           })}
